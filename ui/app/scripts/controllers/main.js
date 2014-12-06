093d71da49a220ed57d78e414d9efe7f25558e96
Shawn Shaw <shawn.e.shaw@gmail.com>
2014-12-06 17:18:38 -0600

    saving progress. drag and drop not working.

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -7,11 +7,76 @@
  * # MainCtrl
  * Controller of the pumpkinApp
  */
-angular.module( 'pumpkinApp', [ 'ngMaterial' ] )
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('pumpkinApp', ['ngMaterial', 'DragAndDrop'])
+    .controller('MainCtrl', [
+        '$scope',
+        function($scope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+
+            this.name = 'Angularjs Drag&Drop';
+
+            this.persons = [{
+                name: 'Ciul',
+                job: 'developer'
+            }, {
+                name: 'Jose',
+                job: 'designer'
+            }, {
+                name: 'Carlos',
+                job: 'sales'
+            }];
+
+            this.animals = [{
+                name: 'Hercules',
+                type: 'dog'
+            }, {
+                name: 'Jimmy',
+                type: 'parrot'
+            }, {
+                name: 'Missy',
+                type: 'cat'
+            }];
+
+            this.status = 'awaiting for something to be dragged :]';
+
+            /**
+             * onDrag
+             */
+            this.onDragStart = angular.bind(this, function(ev) {
+                this.status = 'drag started!';
+            });
+
+            /**
+             * onDragEnter
+             */
+            this.onDragEnter = angular.bind(this, function(ev) {
+                var target = angular.element(ev.target);
+                target.removeClass('drop');
+                target.addClass('over');
+            });
+
+            /**
+             * onDragLeave
+             */
+            this.onDragLeave = angular.bind(this, function(ev) {
+                var target = angular.element(ev.target);
+                target.removeClass('over');
+                target.removeClass('drop');
+            });
+
+            /**
+             * onDrop
+             */
+            this.onDrop = angular.bind(this, function(data, ev) {
+                this.status = 'dropped';
+                this.dropped = data;
+                var target = angular.element(ev.target);
+                target.removeClass('over');
+                target.addClass('drop');
+            });
+        }
+    ]);
