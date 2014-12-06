252a04c6fd7e88d5f11830c73867435e13dbc11e
revanthreddy <revanth.reddy@gmail.com>
2014-12-06 14:21:23 -0600

    compartmentalized

---
+++ websockets/services/connector.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient;
+var db;
+var server = "127.0.0.1";
+
+exports.init = function(){
+    console.log('Initialising DB ...');
+    if(!db){
+        MongoClient.connect("mongodb://"+server+":27017/"+questions, function(err, database) {    
+            if(err) throw err;
+            
+            db = database;
+            console.log('DB setup complete.');
+        });
+    }
+};
+
+exports.getDbConnection = function(){
+    return db;
+};
