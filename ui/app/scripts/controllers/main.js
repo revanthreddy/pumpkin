719174b962531d6d5048bbd810da46ea5d07052a
Shawn Shaw <shawn.e.shaw@gmail.com>
2014-12-06 15:45:59 -0600

    added material design button

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the pumpkinApp
  */
-angular.module('pumpkinApp')
+angular.module( 'pumpkinApp', [ 'ngMaterial' ] )
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
