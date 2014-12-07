27f476c5caeaccca5cc526cff1e334df51bb6a3b
Shawn Shaw <shawn.e.shaw@gmail.com>
2014-12-07 17:34:59 -0600

    images looking better, but can't fix centered content :S

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -8,74 +8,113 @@
  * Controller of the pumpkinApp
  */
 app.controller('MainCtrl', [
-        '$scope',
-        function($scope) {
-            $scope.awesomeThings = [
-                'HTML5 Boilerplate',
-                'AngularJS',
-                'Karma'
-            ];
-
-            this.name = 'Angularjs Drag&Drop';
-
-            this.persons = [{
-                name: 'Ciul',
-                job: 'developer'
+    '$scope', '$http',
+    function($scope, $http) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+ 		
+        
+ 		// HARDCODE
+       	$scope.testInfo = {
+            "_id": 1237287234848,
+            "quiz": {
+                "id": 1234,
+                "title": "Disney Animals and Owners",
+                "terms": [{
+                    "id": 2005281693,
+                    "term": "Abu",
+                    "definition": "Monkey",
+                    "image": {
+                        "url": "https://farm8.staticflickr.com/7054/6930602973_91256bf5fd_m.jpg",
+                        "width": 240,
+                        "height": 160
+                    },
+                    "rank": 0
+                }, {
+                    "id": 2005280218,
+                    "term": "Pluto",
+                    "definition": "Dog",
+                    "image": "https://farm8.staticflickr.com/7054/6930602973_91256bf5fd_m.jpg",
+                    "rank": 1
+                }]
+            },
+            "game_id": 1237287234848,
+            "players": [{
+                "id": 1234,
+                "name": "Milan"
             }, {
-                name: 'Jose',
-                job: 'designer'
-            }, {
-                name: 'Carlos',
-                job: 'sales'
-            }];
+                "id": 1234,
+                "name": "Revanth"
+            }]
+        };
 
-            this.animals = [{
-                name: 'Hercules',
-                type: 'dog'
-            }, {
-                name: 'Jimmy',
-                type: 'parrot'
-            }, {
-                name: 'Missy',
-                type: 'cat'
-            }];
+        $scope.questions = $scope.testInfo.quiz.terms;
+        console.log($scope.questions);
+        // HARDCODE
 
-            this.status = 'awaiting for something to be dragged :]';
+        this.name = 'Angularjs Drag&Drop';
 
-            /**
-             * onDrag
-             */
-            this.onDragStart = angular.bind(this, function(ev) {
-                this.status = 'drag started!';
-            });
+        this.persons = [{
+            name: 'Ciul',
+            job: 'developer'
+        }, {
+            name: 'Jose',
+            job: 'designer'
+        }, {
+            name: 'Carlos',
+            job: 'sales'
+        }];
 
-            /**
-             * onDragEnter
-             */
-            this.onDragEnter = angular.bind(this, function(ev) {
-                var target = angular.element(ev.target);
-                target.removeClass('drop');
-                target.addClass('over');
-            });
+        this.animals = [{
+            name: 'Hercules',
+            type: 'dog'
+        }, {
+            name: 'Jimmy',
+            type: 'parrot'
+        }, {
+            name: 'Missy',
+            type: 'cat'
+        }];
 
-            /**
-             * onDragLeave
-             */
-            this.onDragLeave = angular.bind(this, function(ev) {
-                var target = angular.element(ev.target);
-                target.removeClass('over');
-                target.removeClass('drop');
-            });
+        this.status = 'awaiting for something to be dragged :]';
 
-            /**
-             * onDrop
-             */
-            this.onDrop = angular.bind(this, function(data, ev) {
-                this.status = 'dropped';
-                this.dropped = data;
-                var target = angular.element(ev.target);
-                target.removeClass('over');
-                target.addClass('drop');
-            });
-        }
-    ]);
+        /**
+         * onDrag
+         */
+        this.onDragStart = angular.bind(this, function(ev) {
+            this.status = 'drag started!';
+        });
+
+        /**
+         * onDragEnter
+         */
+        this.onDragEnter = angular.bind(this, function(ev) {
+            var target = angular.element(ev.target);
+            target.removeClass('drop');
+            target.addClass('over');
+        });
+
+        /**
+         * onDragLeave
+         */
+        this.onDragLeave = angular.bind(this, function(ev) {
+            var target = angular.element(ev.target);
+            target.removeClass('over');
+            target.removeClass('drop');
+        });
+
+        /**
+         * onDrop
+         */
+        this.onDrop = angular.bind(this, function(data, ev) {
+            this.status = 'dropped';
+            this.dropped = data;
+            var target = angular.element(ev.target);
+            target.removeClass('over');
+            target.addClass('drop');
+        });
+    }
+]);
